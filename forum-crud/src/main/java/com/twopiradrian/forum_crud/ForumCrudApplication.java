36dc43da0e5a63eb7864d21669b455ebb9eb9e2a
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-12-04 21:30:40 -0300

    project init

---
+++ forum-crud/src/main/java/com/twopiradrian/forum_crud/ForumCrudApplication.java
@@ -0,0 +1,13 @@
+package com.twopiradrian.forum_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumCrudApplication.class, args);
+	}
+
+}
---
+++ forum-crud/src/test/java/com/twopiradrian/forum_crud/ForumCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twopiradrian.forum_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
