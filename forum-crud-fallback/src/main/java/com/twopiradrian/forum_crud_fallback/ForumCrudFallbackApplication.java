87792624fa7cd07f0a4056c6308fad2d5efd4c74
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-12-14 12:47:26 -0300

    forum fallback

---
+++ forum-crud-fallback/src/main/java/com/twopiradrian/forum_crud_fallback/ForumCrudFallbackApplication.java
@@ -0,0 +1,13 @@
+package com.twopiradrian.forum_crud_fallback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumCrudFallbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumCrudFallbackApplication.class, args);
+	}
+
+}
---
+++ forum-crud-fallback/src/test/java/com/twopiradrian/forum_crud_fallback/ForumCrudFallbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twopiradrian.forum_crud_fallback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumCrudFallbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
