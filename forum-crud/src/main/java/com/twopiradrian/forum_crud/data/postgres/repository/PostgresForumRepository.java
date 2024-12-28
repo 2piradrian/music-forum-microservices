815613602c6c644ad5abc666c28cb318a0aa021a
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-12-28 19:49:17 -0300

    monthly query

--- forum-crud/src/main/java/com/twopiradrian/forum_crud/data/postgres/repository/PostgresForumRepository.java
+++ forum-crud/src/main/java/com/twopiradrian/forum_crud/data/postgres/repository/PostgresForumRepository.java
@@ -1,7 +1,6 @@
 package com.twopiradrian.forum_crud.data.postgres.repository;
 
 import com.twopiradrian.forum_crud.data.postgres.model.ForumModel;
-import com.twopiradrian.forum_crud.domain.entity.Forum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,23 +11,31 @@ import java.util.List;
 
 public interface PostgresForumRepository extends JpaRepository<ForumModel, String> {
 
-    @Query("SELECT f FROM ForumModel f WHERE f.status <> :status ORDER BY f.createdAt DESC")
+    @Query(
+            value = "SELECT f FROM ForumModel f WHERE f.status <> :status ORDER BY f.createdAt DESC"
+    )
     Page<ForumModel> findAll(
             @Param("status")
             String status,
             Pageable pageable
     );
 
-    @Query("SELECT f FROM ForumModel f WHERE f.category = :category AND f.status <> :status ORDER BY f.createdAt DESC")
+    @Query(
+            value = "SELECT f FROM ForumModel f WHERE f.category = :category " +
+                    "AND f.status <> :status ORDER BY f.createdAt DESC"
+    )
     Page<ForumModel> findAllByCategory(
             @Param("category") String category,
             @Param("status") String status,
             Pageable pageable
     );
 
-    @Query("SELECT f FROM ForumModel f WHERE FUNCTION('EXTRACT', 'MONTH', f.createdAt) = :month " +
-            "AND FUNCTION('EXTRACT', 'YEAR', f.createdAt) = :year " +
-            "AND f.status <> :status ORDER BY f.createdAt DESC")
+    @Query(
+            value = "SELECT f FROM ForumModel f WHERE EXTRACT(MONTH FROM f.createdAt) = :month " +
+                    "AND EXTRACT(YEAR FROM f.createdAt) = :year " +
+                    "AND f.status <> :status ORDER BY f.createdAt DESC",
+            nativeQuery = true
+    )
     List<ForumModel> getMonthlyForums(
             @Param("month") int month,
             @Param("year") int year,
