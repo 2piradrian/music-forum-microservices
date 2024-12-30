70277e3d90cfc9e8f0d61b58c43e093b8cdbd6c6
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-12-30 13:28:43 -0300

    gateway comment route

--- gateway/src/main/java/com/twopiradrian/gateway/beans/GatewayBeans.java
+++ gateway/src/main/java/com/twopiradrian/gateway/beans/GatewayBeans.java
@@ -17,14 +17,19 @@ public class GatewayBeans {
                         .uri("lb://forum-server")
                 )
                 .route(r -> r
-                        .path("/api/report/**")
-                        .filters(f -> f.rewritePath("/api/report/(?<segment>.*)", "/report-server/api/report/${segment}"))
-                        .uri("lb://report-server")
+                        .path("/api/comment/**")
+                        .filters(f -> f.rewritePath("/api/comment/(?<segment>.*)", "/forum-server/api/comment/${segment}"))
+                        .uri("lb://forum-server")
                 )
                 .route(r -> r
                         .path("/api/users/**")
                         .filters(f -> f.rewritePath("/api/users/(?<segment>.*)", "/auth-server/api/users/${segment}"))
                         .uri("lb://auth-server")
+                )
+                .route(r -> r
+                        .path("/api/report/**")
+                        .filters(f -> f.rewritePath("/api/report/(?<segment>.*)", "/report-server/api/report/${segment}"))
+                        .uri("lb://report-server")
                 )
                 .build();
     }
