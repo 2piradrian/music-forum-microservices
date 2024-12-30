9eaee61ac9b75ad85b8e0b9a039c3787eeaba558
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-12-30 20:21:29 -0300

    enhance run script with clearer instructions and suppress output for service builds

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-# Script to build and run all services in the correct order
-## You need to have: maven, java 21, docker and docker compose
+# Script to build and run all services
+
+# to run this script: chmod +x run.sh && sh ./run.sh
+# (remember run before: docker compose up)
+
+# You need to have installed: maven, java 21
+# if you are a windows user, you will need to install all of this in your wsl
+
+clear
+echo "Building and starting all services"
+echo "This may take a while..."
+echo
 
 dependencies=(
     common
@@ -9,18 +19,18 @@ dependencies=(
 
 for dependency in "${dependencies[@]}"; do
         echo "Building and installing $dependency"
-        (cd "./$dependency" && mvn package)
-        (cd "./$dependency" && mvn install)
+        (cd "./$dependency" && mvn package) > /dev/null 2>&1
+        (cd "./$dependency" && mvn install) > /dev/null 2>&1
 done
 
 services=(
     registry-server 
     config-server
-    )
+)
 
 for service in "${services[@]}"; do
     echo "Starting $service"
-    (cd "./$service" && mvn spring-boot:run) &
+    (cd "./$service" && mvn spring-boot:run > /dev/null 2>&1) &
     sleep 20
 done
 
@@ -30,12 +40,26 @@ services=(
     report-listener
     report-server
     gateway
-    )
+)
 
 for service in "${services[@]}"; do
     echo "Starting $service"
-    (cd "./$service" && mvn spring-boot:run) &
+    (cd "./$service" && mvn spring-boot:run > /dev/null 2>&1) &
     sleep 5
 done
+
+echo
+
+for i in {1..20}; do
+    sleep 1
+    echo -n "."
+done
+
+clear
+
+echo "If everything went well, the services should be running... Do not close this terminal."
+echo "To stop all services, press Ctrl + C"
+echo
+echo "Developed by: 2piradrian"
 
 wait
