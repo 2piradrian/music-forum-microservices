d0efc316418b118c7e81eab919b20329efeb9491
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-12-30 22:17:53 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,22 +2,37 @@
 
 # Script to build and run all services
 
-# to run this script: chmod +x run.sh && sh ./run.sh
-# (remember run before: docker compose up)
+# to run this script: chmod +x run.sh && bash ./run.sh
 
 # You need to have installed: maven, java 21
 # if you are a windows user, you will need to install all of this in your wsl
 
+await() {
+    number=$1
+
+    i=0
+    echo ""
+    while [ "$i" -lt "$number" ]; do
+        echo -n "."
+        i=$((i + 1))
+        sleep 1
+    done
+    echo ""
+}
+
 clear
 echo "Building and starting all services"
 echo "This may take a while..."
-echo
+
+(docker compose up > /dev/null 2>&1) &
+await 20
 
 dependencies=(
     common
 )
 
 for dependency in "${dependencies[@]}"; do
+        echo ""
         echo "Building and installing $dependency"
         (cd "./$dependency" && mvn package) > /dev/null 2>&1
         (cd "./$dependency" && mvn install) > /dev/null 2>&1
@@ -29,9 +44,10 @@ services=(
 )
 
 for service in "${services[@]}"; do
+    echo ""
     echo "Starting $service"
     (cd "./$service" && mvn spring-boot:run > /dev/null 2>&1) &
-    sleep 20
+    await 20
 done
 
 services=(
@@ -43,23 +59,19 @@ services=(
 )
 
 for service in "${services[@]}"; do
+    echo ""
     echo "Starting $service"
     (cd "./$service" && mvn spring-boot:run > /dev/null 2>&1) &
-    sleep 5
+    await 5
 done
 
-echo
-
-for i in {1..20}; do
-    sleep 1
-    echo -n "."
-done
+await 20
 
 clear
 
 echo "If everything went well, the services should be running... Do not close this terminal."
 echo "To stop all services, press Ctrl + C"
-echo
+echo ""
 echo "Developed by: 2piradrian"
 
 wait
